a6dbda47992c359a4af0c10d78459ba60475cacc
Nguyen Hao <haont2901@gmail.com>
2023-03-29 10:55:45 +0700

    Apply WebDriver Manager

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -4,32 +4,57 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.opera.OperaDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	private WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
-protected WebDriver getBrowserDriver(String browserName) {
-	switch (browserName) {
-	case "firefox":
-		System.setProperty("webdriver.gecko.driver", projectPath+"\\browserDriver\\geckodriver.exe");
-		driver = new FirefoxDriver();
-		break;
-	case "chrome":
-		System.setProperty("webdriver.chrome.driver", projectPath+"\\browserDriver\\chromedriver.exe");
-		driver = new ChromeDriver();
-		break;
-	case "edge":
-		System.setProperty("webdriver.edge.driver", projectPath+"\\browserDriver\\msedgedriver.exe");
-		driver = new EdgeDriver();
-		break;
 
-	default: throw new RuntimeException("Browser Name is invalid");
+	protected WebDriver getBrowserDriver(String browserName) {
+		switch (browserName) {
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			break;
+
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;
+		case "opera":
+			WebDriverManager.operadriver().setup();
+			driver = new OperaDriver();
+			break;
+		case "coccoc": {
+			WebDriverManager.chromedriver().driverVersion("").setup();
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files (x86)\\Microsoft\\coccoc\\Application\\coccoc.exe");
+			driver = new ChromeDriver(options);
+			break;
+		}
+		case "brave":
+			WebDriverManager.chromedriver().driverVersion("").setup();
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files (x86)\\Microsoft\\Brave\\Application\\brave.exe");
+			driver = new ChromeDriver(options);
+			break;
+
+		default:
+			throw new RuntimeException("Browser Name is invalid");
+		}
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		return driver;
+
 	}
-	driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-	driver.manage().window().maximize();
-	return driver;
-	
-}
 }
