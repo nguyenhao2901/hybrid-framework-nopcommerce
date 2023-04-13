966844198c9f95d30897e3153d55c407e8d7160d
Nguyen Hao <haont2901@gmail.com>
2023-04-13 21:42:59 +0700

    Extent report version 5

--- actions/commons/GlobalConstants.java
+++ actions/commons/GlobalConstants.java
@@ -13,5 +13,7 @@ public class GlobalConstants {
 	public static final long LONG_TIMEOUT = 30;
 	public static final long SHORT_TIMEOUT = 5;
 	public static final String EXTENT_PATH = System.getProperty("user.dir") + File.separator + "ExtentReportV2" + File.separator  ;
+	public static final String PROJECT_PATH = System.getProperty("user.dir");
+	public static final String JAVA_VERSION = System.getProperty("java.version");
 
 }
